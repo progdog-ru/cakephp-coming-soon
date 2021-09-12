df657c9d11aff95efabf76b6cfe30b23de9dcd67
Evgeniy Kuskov <68942249+cakephp-ru@users.noreply.github.com>
2021-09-12 15:07:22 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# cakephp-coming-soon
- ComingSoon plugin for CakePHP
+# ComingSoon plugin for CakePHP
+
+## Installation
+
+You can install this plugin into your CakePHP application using [composer](https://getcomposer.org).
+
+The recommended way to install composer packages is:
+
+```
+composer require your-name-here/coming-soon
+```
---
+++ src/Middleware/ComingSoonMiddleware.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace ComingSoon\Middleware;
+
+use Cake\Core\InstanceConfigTrait;
+use Cake\Utility\Inflector;
+use Cake\View\ViewBuilder;
+use Cake\Core\Configure;
+
+class ComingSoonMiddleware
+{
+    use InstanceConfigTrait;
+
+    /**
+     * Default config.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'className' => 'Cake\View\View',
+        'templatePath' => 'ComingSoon',
+
+        'configPath' => 'Settings.is_coming_soon',
+        'statusCode' => 200,
+
+        'templateFileName' => 'coming_soon',
+        'layout' => 'default',
+
+        'contentType' => 'text/html'
+    ];
+
+    /**
+     * 
+     */
+    public function __construct($config = [])
+    {
+        $this->setConfig($config);
+    }
+
+    /**
+     * 
+     */
+    public function __invoke($request, $response, $next)
+    {
+        $isActive = $this->isComingSoon($request);
+
+        if ($isActive === false) {
+            $response = $next($request, $response);
+        } else {
+            $response = $this->execute($response);
+        }
+
+        return $response;
+    }
+
+    /**
+     * 
+     */
+    private function execute($response)
+    {
+        $cakeRequest = \Cake\Http\ServerRequestFactory::fromGlobals();
+        $builder = new ViewBuilder();
+
+        $className = $this->getConfig('className');
+        $templateName = $this->getConfig('templateFileName');
+        $templatePath = $this->getConfig('templatePath');
+        $contentType = $this->getConfig('contentType');
+        $statusCode = $this->getConfig('statusCode');
+        $layout = $this->getConfig('layout');
+
+        $view = $builder
+            ->setClassName($className)
+            ->setTemplatePath(Inflector::camelize($templatePath));
+
+
+        if ($layout) {
+            $view = $view->setLayout($layout);
+        } else {
+            $view = $view->disableAutoLayout();
+        }
+
+        $view = $view->build([], $cakeRequest);
+
+        $bodyString = $view->render($templateName);
+
+        $response = $response
+            ->withHeader('Content-Type', $contentType)
+            ->withStatus($statusCode);
+        $response
+            ->getBody()
+            ->write($bodyString);
+
+        return $response;
+    }
+
+    /**
+     * 
+     */
+    private function isComingSoon($request)
+    {
+        $ret = $this->checkStatus();
+        if ($ret === false) {
+            return false;
+        }
+
+        $ret = $this->isAllow($request);
+        if ($ret === true) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     * 
+     */
+    private function checkStatus()
+    {
+        $path = $this->getConfig('configPath');
+        $result = Configure::read($path);
+        if ($result === null) {
+            $result = false;
+        }
+
+        return $result;
+    }
+
+    /**
+     * TODO: проверить залогинен ли пользователь или запрос идет ли на страницу входа на сайт
+     */
+    private function isAllow($request)
+    {
+        return false;
+    }
+}
